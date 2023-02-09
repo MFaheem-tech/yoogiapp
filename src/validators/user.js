59814970699cc7a4abfae68f80111a8a547c52e8
Muhammad Arslan <muhammadarslan6492@gmail.com>
2023-02-09 20:26:20 +0500

    yogi project starter

--- src/validators/user.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-export const schema = {
-  register: Joi.object({
-    name: Joi.string().trim().required(),
-    email: Joi.string().email().trim().required(),
-    password: Joi.string().alphanum().min(6).max(64).trim().required(),
-  }),
-};
